38254ecb7a7e73c0a026f41a77e35ad2a2ae0b8a
Karthick Chinnathambi <karthickc@quickplay.com>
2017-11-19 19:48:15 +0530

    Tutorial 5: Handling Row Selection

--- 2_SimpleTableView/2_SimpleTableView/Universal/SimpleTableViewController.m
+++ 2_SimpleTableView/2_SimpleTableView/Universal/SimpleTableViewController.m
@@ -13,6 +13,7 @@
     NSArray *tableContents;
     NSArray *thumbNails;
     NSArray *prepTimes;
+    NSMutableArray<NSNumber *> *selections;
 }
 @end
 
@@ -33,6 +34,10 @@
     tableContents = [recipiesInfo objectForKey:@"name"];
     thumbNails = [recipiesInfo objectForKey:@"thumbnail"];
     prepTimes = [recipiesInfo objectForKey:@"prep_time"];
+    selections = [[NSMutableArray alloc] init];
+    for(int i=0; i < tableContents.count; i++) {
+        selections[i] = [NSNumber numberWithBool:NO];
+    }
 #endif
 }
 
@@ -42,11 +47,10 @@
     // Dispose of any resources that can be recreated.
 }
 
-#pragma mark UITableViewDataSource
+#pragma mark Delegates
+
 #define DISABLE_CUSTOM_TABLE_VIEW_CELL 0
-- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
-    return 1;
-}
+#pragma mark -UITableViewDelegate
 
 -(CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
 #if DISABLE_CUSTOM_TABLE_VIEW_CELL
@@ -56,6 +60,30 @@
 #endif
 }
 
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    UIAlertController *recipieAlert = [UIAlertController alertControllerWithTitle:[tableContents objectAtIndex:indexPath.row]
+                                                                          message:[NSString stringWithFormat:@"Can be prepared in %@",[prepTimes objectAtIndex:indexPath.row]]
+                                                                   preferredStyle:UIAlertControllerStyleAlert];
+    __weak UITableViewCell *weakCurrentCell = [tableView cellForRowAtIndexPath:indexPath];
+    UIAlertAction *action = [UIAlertAction actionWithTitle:@"OK"
+                                                     style:UIAlertActionStyleCancel
+                                                   handler:^void(UIAlertAction *action){
+                                                       UITableViewCell *currentCell = weakCurrentCell;
+                                                       if(selections != nil) {
+                                                           BOOL currentSelStatus = [selections[indexPath.row] boolValue];
+                                                           selections[indexPath.row] = [NSNumber numberWithBool:!currentSelStatus];
+                                                           [self updateCellAccessoryType:currentCell atIndexPath:indexPath];
+                                                       }
+                                                   }];
+    [recipieAlert addAction:action];
+    [self presentViewController:recipieAlert animated:YES completion:nil];
+}
+
+#pragma mark -UITableViewDataSource
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;
+}
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
     return [tableContents count];
 }
@@ -94,7 +122,17 @@
         rowCell.imageView.image = [UIImage imageNamed:[thumbNails objectAtIndex:indexPath.row]];
         //Default UITableCellView Block -End
     }
+    [self updateCellAccessoryType:rowCell atIndexPath:indexPath];
 }
 
-
+- (void) updateCellAccessoryType: (UITableViewCell *) cell atIndexPath: (NSIndexPath *) indexPath {
+    if(cell == nil) {
+        return;
+    }
+    if([selections[indexPath.row] boolValue] == YES) {
+        cell.accessoryType = UITableViewCellAccessoryCheckmark;
+    }else {
+        cell.accessoryType = UITableViewCellAccessoryNone;
+    }
+}
 @end
