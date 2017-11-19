4e5156e285f89ce6a9c1250da502e0b96712267f
Karthick Chinnathambi <karthickc@quickplay.com>
2017-11-19 09:43:22 +0530

    Tutorial2: Adding Thumbnail Assets

--- 2_SimpleTableView/2_SimpleTableView/Universal/SimpleTableViewController.m
+++ 2_SimpleTableView/2_SimpleTableView/Universal/SimpleTableViewController.m
@@ -10,6 +10,7 @@
 
 @interface SimpleTableViewController () {
     NSArray *tableContents;
+    NSArray *thumbNails;
 }
 @end
 
@@ -19,6 +20,8 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     tableContents = [NSArray arrayWithObjects:@"Egg Benedict", @"Mushroom Risotto", @"Full Breakfast", @"Hamburger", @"Ham and Egg Sandwich", @"Creme Brelee", @"White Chocolate Donut", @"Starbucks Coffee", @"Vegetable Curry", @"Instant Noodle with Egg", @"Noodle with BBQ Pork", @"Japanese Noodle with Pork", @"Green Tea", @"Thai Shrimp Cake", @"Angry Birds Cake", @"Ham and Cheese Panini", nil];
+    thumbNails = [NSArray arrayWithObjects:@"egg_benedict.jpg", @"mushroom_risotto.jpg", @"full_breakfast.jpg", @"hamburger.jpg", @"ham_and_egg_sandwich.jpg", @"creme_brelee.jpg", @"white_chocolate_donut.jpg", @"starbucks_coffee.jpg", @"vegetable_curry.jpg", @"instant_noodle_with_egg.jpg", @"noodle_with_bbq_pork.jpg", @"japanese_noodle_with_pork.jpg", @"green_tea.jpg", @"thai_shrimp_cake.jpg", @"angry_birds_cake.jpg", @"ham_and_cheese_panini.jpg", nil];
+
 
 }
 
@@ -44,7 +47,11 @@
         currentCell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:simpleTableIdentifier];
     }
     currentCell.textLabel.text = [tableContents objectAtIndex:indexPath.row];
+#if 0
     currentCell.imageView.image = [UIImage imageNamed:@"creme_brelee.jpg"];
+#else
+    currentCell.imageView.image = [UIImage imageNamed:[thumbNails objectAtIndex:indexPath.row]];
+#endif
     return currentCell;
 }
 
