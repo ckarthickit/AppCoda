f0513f236dee6417f3659bebe675acc26982d334
Karthick Chinnathambi <karthickc@quickplay.com>
2018-01-08 20:03:24 +0530

    Camera App Using AV Foundation: Initial Commit

---
+++ 101_CameraApp-AVFoundation/CameraApp-AVFoundation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CameraApp-AVFoundation
+//
+//  Created by Karthick C on 08/01/18.
+//  Copyright © 2018 Karthick C. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
